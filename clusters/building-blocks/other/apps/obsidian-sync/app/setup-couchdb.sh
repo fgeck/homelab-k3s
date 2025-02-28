17c12a7ac1b4eefe002f5d26f51d242eebf6cb86
Florian Geckeler <f.geckeler@sap.com>
2025-02-28 12:52:25 +0100

    Add couchdb setup script

---
+++ clusters/building-blocks/other/apps/obsidian-sync/app/setup-couchdb.sh
@@ -0,0 +1,10 @@
+# on the first startup exec into the couchdb pod and execute following:
+export hostname=localhost:5984 ; export username=$COUCHDB_USER ; export password=$COUCHDB_PASSWORD ; curl -s https://raw.githubusercontent.com/vrtmrz/obsidian-livesync/main/utils/couchdb/couchdb-init.sh | bash
+
+# Once done and couchdb is reachable via internet, execute following on a local machine:
+export hostname=https://obsidian.<DOMAIN_NAME>
+export database=obsidiannotes
+export passphrase=<RANDUM STRING>
+export username=$COUCHDB_USER
+export password=$COUCHDB_PASSWORD
+deno run -A https://raw.githubusercontent.com/vrtmrz/obsidian-livesync/main/utils/flyio/generate_setupuri.ts
